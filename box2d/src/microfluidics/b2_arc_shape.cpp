98df690c1d65f427c54e887fc9b8a4652e459ec9
quarz12 <danielmartinqwe@gmail.com>
2024-10-12 17:56:08 +0200

    arcshape use radians 2

--- box2d/src/microfluidics/b2_arc_shape.cpp
+++ box2d/src/microfluidics/b2_arc_shape.cpp
@@ -8,71 +8,61 @@
 #include <new>
 
 
-void b2ArcShape::SetTwoSided(const b2Vec2& center, const b2Vec2& start, const b2Vec2& end)
-{
+void b2ArcShape::SetTwoSided(const b2Vec2& center, const b2Vec2& start, const b2Vec2& end) {
     m_vertex1 = start;
     m_vertex2 = end;
-    m_center=center;
-    m_radius=(start-center).Length();
-    fromRadians=b2Atan2(start.y-center.y, start.x-center.x);
-    toRadians=b2Atan2(end.y-center.y, end.x-center.x);
+    m_center = center;
+    m_radius = (start - center).Length();
+    fromRadians = b2Atan2(start.y-center.y, start.x-center.x);
+    toRadians = b2Atan2(end.y-center.y, end.x-center.x);
 }
 
-void b2ArcShape::SetTwoSided(const b2Vec2& center, const float startRadians, const float endRadians, const float radius) {
-    m_vertex1.x = center.x +radius*cos(fromRadians);
-    m_vertex1.y = center.y +radius*sin(fromRadians);
-    m_vertex2.x = center.x +radius*cos(toRadians);
-    m_vertex2.y = center.y +radius*sin(toRadians);
-    m_center=center;
-    fromRadians=startRadians;
-    toRadians=endRadians;
-    m_radius=radius;
+void b2ArcShape::SetTwoSided(const b2Vec2& center, const float startRadians, const float endRadians,
+                             const float radius) {
+    m_vertex1.x = center.x + radius * cos(fromRadians);
+    m_vertex1.y = center.y + radius * sin(fromRadians);
+    m_vertex2.x = center.x + radius * cos(toRadians);
+    m_vertex2.y = center.y + radius * sin(toRadians);
+    m_center = center;
+    fromRadians = startRadians;
+    toRadians = endRadians;
+    m_radius = radius;
 }
 
-b2Shape* b2ArcShape::Clone(b2BlockAllocator* allocator) const
-{
+b2Shape* b2ArcShape::Clone(b2BlockAllocator* allocator) const {
     void* mem = allocator->Allocate(sizeof(b2ArcShape));
-    b2ArcShape* clone = new (mem) b2ArcShape;
+    b2ArcShape* clone = new(mem) b2ArcShape;
     *clone = *this;
     return clone;
 }
 
-int32 b2ArcShape::GetChildCount() const
-{
+int32 b2ArcShape::GetChildCount() const {
     return 1;
 }
 
-bool b2ArcShape::TestPoint(const b2Transform& xf, const b2Vec2& p) const
-{
+bool b2ArcShape::TestPoint(const b2Transform& xf, const b2Vec2& p) const {
     B2_NOT_USED(xf);
     B2_NOT_USED(p);
     return false;
 }
 
-void b2ArcShape::ComputeDistance(const b2Transform& transform, const b2Vec2& point, float* distance, b2Vec2* normal, int32 childIndex) const
-{
+void b2ArcShape::ComputeDistance(const b2Transform& transform, const b2Vec2& point, float* distance, b2Vec2* normal,
+                                 int32 childIndex) const {
     B2_NOT_USED(childIndex);
 
     b2Vec2 center = transform.p + b2Mul(transform.q, m_center); // Center of the circle
 
     // Check if the closest point is within the arc
     b2Vec2 toPoint = point - center; // Vector from center to point point
-    b2Vec2 toStart = m_vertex1 - center;
-    b2Vec2 toEnd = m_vertex2 - center;
 
-    float distanceToPoint=toPoint.Length();
-
-    // Normalize the vectors to get directions
-    toPoint.Normalize();
-    toStart.Normalize();
-    toEnd.Normalize();
+    float distanceToPoint = toPoint.Length();
 
     double angleToPoint = b2Atan2(toPoint.y, toPoint.x);
     double angleToStart = fromRadians;
     double angleToEnd = toRadians;
-    angleToEnd=b2_pi-angleToEnd; //flip yaxis todo remove if renderer changes?
-    angleToStart=b2_pi-angleToStart;
-    angleToPoint=b2_pi-angleToPoint;
+    // angleToEnd = b2_pi - angleToEnd; //flip yaxis todo remove if renderer changes?
+    // angleToStart = b2_pi - angleToStart;
+    // angleToPoint = b2_pi - angleToPoint;
     // Normalize angles to [0, 2π]
     if (angleToPoint < 0)
         angleToPoint += 2.0f * b2_pi;
@@ -82,33 +72,28 @@ void b2ArcShape::ComputeDistance(const b2Transform& transform, const b2Vec2& poi
         angleToEnd += 2.0f * b2_pi;
 
     bool isWithinArc;
-    if (angleToStart > angleToEnd) {
-        isWithinArc = (angleToPoint <= angleToStart && angleToPoint >= angleToEnd);
+    if (angleToStart < angleToEnd) {
+        isWithinArc = angleToStart <= angleToPoint && angleToPoint <= angleToEnd;
     } else {
-        isWithinArc = (angleToPoint > angleToStart && angleToPoint >= angleToEnd);
+        isWithinArc = (angleToStart <= angleToPoint && angleToPoint < 2 * b2_pi) || (
+                          angleToPoint <= angleToEnd && angleToPoint > 0);
     }
 
-    if (isWithinArc)
-    {
-        *distance = m_radius-distanceToPoint;
-        *normal = *distance>0 ? -toPoint : toPoint;
+    if (isWithinArc) {
+        *distance = m_radius - distanceToPoint;
+        *normal = *distance > 0 ? -toPoint : toPoint;
         *distance = fabs(*distance);
-    }
-    else
-    {
+    } else {
         // Compute distance to the closest endpoint of the arc
-        b2Vec2 pointToStart=point- m_vertex1;
-        b2Vec2 pointToEnd= point - m_vertex2;
+        b2Vec2 pointToStart = point - m_vertex1;
+        b2Vec2 pointToEnd = point - m_vertex2;
         float distanceToStart = pointToStart.Normalize();
         float distanceToEnd = pointToEnd.Normalize();
 
-        if (distanceToStart < distanceToEnd)
-        {
+        if (distanceToStart < distanceToEnd) {
             *distance = distanceToStart;
             *normal = pointToStart;
-        }
-        else
-        {
+        } else {
             *distance = distanceToEnd;
             *normal = pointToEnd;
         }
@@ -118,52 +103,46 @@ void b2ArcShape::ComputeDistance(const b2Transform& transform, const b2Vec2& poi
 
 /// check if input ray hits the shape
 bool b2ArcShape::RayCast(b2RayCastOutput* output, const b2RayCastInput& input,
-                         const b2Transform& transform, int32 childIndex) const
-{
+                         const b2Transform& transform, int32 childIndex) const {
     B2_NOT_USED(childIndex);
-    b2Vec2 position = transform.p + b2Mul(transform.q, m_center);
-    b2Vec2 s = input.p1 - position; //circle center to ray start
+    b2Vec2 center = transform.p + b2Mul(transform.q, m_center);
+    b2Vec2 s = input.p1 - center; //circle center to ray start
     float b = b2Dot(s, s) - m_radius * m_radius;
 
     // Solve quadratic equation.
     b2Vec2 r = input.p2 - input.p1;
-    float c =  b2Dot(s, r);
+    float c = b2Dot(s, r);
     float rr = b2Dot(r, r);
     float sigma = c * c - rr * b;
 
     // Check for negative discriminant and short segment.
     // no intersection
-    if (sigma < 0.0f || rr < b2_epsilon)
-    {
+    if (sigma < 0.0f || rr < b2_epsilon) {
         return false;
     }
 
     // Find the point of intersection of the line with the circle.
     float fractionOfRayUntilCollision = -(c + b2Sqrt(sigma));
-    if(fractionOfRayUntilCollision < 0)
+    if (fractionOfRayUntilCollision < 0)
         fractionOfRayUntilCollision = -(c - b2Sqrt(sigma));
     // Is the intersection point on the segment?
     if (0.0f <= fractionOfRayUntilCollision && fractionOfRayUntilCollision <= input.maxFraction * rr) {
         fractionOfRayUntilCollision /= rr; //✓
-        b2Vec2 intersectionPoint = input.p1 + fractionOfRayUntilCollision * r;//✓
+        b2Vec2 intersectionPoint = input.p1 + fractionOfRayUntilCollision * r; //✓
 
         // Check if the intersection point is within the arc segment
-        b2Vec2 toIntersection = intersectionPoint - position;
-        b2Vec2 toStart = m_vertex1 - position;
-        b2Vec2 toEnd = m_vertex2 - position;
-        float distanceToStart=toStart.Length();
+        b2Vec2 toIntersection = intersectionPoint - center;
+        float distanceToStartPoint = s.Length();
         // Normalize the vectors to get directions
         toIntersection.Normalize();
-        toStart.Normalize();
-        toEnd.Normalize();
 
         // Calculate angles in radian
-        double angleToIntersection = b2Atan2(toIntersection.y,toIntersection.x);
+        double angleToIntersection = b2Atan2(toIntersection.y, toIntersection.x);
         double angleToStart = fromRadians;
         double angleToEnd = toRadians;
-        angleToEnd=b2_pi-angleToEnd; //flip yaxis todo remove if renderer changes?
-        angleToStart=b2_pi-angleToStart;
-        angleToIntersection=b2_pi-angleToIntersection;
+        // angleToEnd=b2_pi-angleToEnd; //flip yaxis todo remove if renderer changes?
+        // angleToStart=b2_pi-angleToStart;
+        // angleToIntersection=b2_pi-angleToIntersection;
         // Normalize angles to [0, 2π]
         if (angleToIntersection < 0)
             angleToIntersection += 2 * b2_pi;
@@ -173,40 +152,29 @@ bool b2ArcShape::RayCast(b2RayCastOutput* output, const b2RayCastInput& input,
             angleToEnd += 2 * b2_pi;
 
         bool isWithinArc;
-
-        if (angleToStart > angleToEnd) {
-            isWithinArc = (angleToIntersection <= angleToStart && angleToIntersection >= angleToEnd);
+        print("angle to end:" + str(angleToEnd));
+        print("angle to start:" + str(angleToStart));
+        print("angle to intersection:" + str(angleToIntersection));
+        if (angleToStart < angleToEnd) {
+            isWithinArc = angleToStart <= angleToIntersection && angleToIntersection <= angleToEnd;
         } else {
-            isWithinArc = (angleToIntersection > angleToStart && angleToIntersection >= angleToEnd);
+            isWithinArc = (angleToStart <= angleToIntersection && angleToIntersection <= 2 * b2_pi) || (
+                              angleToEnd >= angleToIntersection && angleToIntersection >= 0);
         }
-
-//        EM_ASM({
-//            console.log("ray from: "+$0+","+$1);
-//            console.log("ray to: "+$2+","+$3);
-//            console.log("angle to start: "+$4);
-//            console.log("angle to end: "+$5);
-//            console.log("angle to intersection: "+$6);
-//                   if ($8 > 2 && $9 > 2)
-//                       console.log("point of intersection: " + $8 + "," + $9);
-//            console.log("iswithin: "+Boolean($7));
-//               }, input.p1.x, input.p1.y, input.p2.x, input.p2.y, angleToStart, angleToEnd, angleToIntersection,
-//               isWithinArc, intersectionPoint.x, intersectionPoint.y
-//        );
-
-        b2Vec2 surfaceNorm=(intersectionPoint-position);
+        print("isWithinArc:" + str(isWithinArc));
+        b2Vec2 surfaceNorm = intersectionPoint - center;
         surfaceNorm.Normalize();
         if (isWithinArc) {
-            output->fraction =  fractionOfRayUntilCollision;
-            output->normal = distanceToStart<m_radius ? surfaceNorm : -surfaceNorm;
-//            EM_ASM(console.log("surface norm: "+$0+","+$1),output->normal.x,output->normal.y);
+            output->fraction = fractionOfRayUntilCollision;
+            output->normal = distanceToStartPoint > m_radius ? surfaceNorm : -surfaceNorm;
+            print(output->normal.ToString());
             return true;
         }
     }
     return false;
 }
 
-void b2ArcShape::ComputeAABB(b2AABB* aabb, const b2Transform& transform, int32 childIndex) const
-{
+void b2ArcShape::ComputeAABB(b2AABB* aabb, const b2Transform& transform, int32 childIndex) const {
     B2_NOT_USED(childIndex);
 
     b2Vec2 p = transform.p + b2Mul(transform.q, m_center);
@@ -215,47 +183,47 @@ void b2ArcShape::ComputeAABB(b2AABB* aabb, const b2Transform& transform, int32 c
     //TODO can be optimized for arcs <= 3/4 circle, careful with start<end
 }
 
-void b2ArcShape::ComputeMass(b2MassData* massData, float density) const
-{
+void b2ArcShape::ComputeMass(b2MassData* massData, float density) const {
     B2_NOT_USED(density);
 
     massData->mass = 0.0f;
-    massData->center=0.5f*(m_vertex1 - m_vertex2);
+    massData->center = 0.5f * (m_vertex1 - m_vertex2);
     massData->I = 0.0f;
 }
+
 bool b2ArcShape::CloserToNext(b2Vec2& point, b2Transform& tf) const {
-    if(nextSegment== nullptr)
+    if (nextSegment == nullptr)
         return false;
     float distanceThis, distanceNext;
-    b2Vec2 normThis,normNext;
-    b2Transform transform=tf;
+    b2Vec2 normThis, normNext;
+    b2Transform transform = tf;
     ComputeDistance(transform, point, &distanceThis, &normThis, 0);
-    nextSegment->ComputeDistance(transform, point, &distanceNext, &normNext,0);
-    if (distanceNext==distanceThis){
+    nextSegment->ComputeDistance(transform, point, &distanceNext, &normNext, 0);
+    if (distanceNext == distanceThis) {
         return this < nextSegment;
     }
-    return distanceNext<distanceThis;
+    return distanceNext < distanceThis;
     //TODO check Angle
 }
 
 bool b2ArcShape::CloserToPrev(b2Vec2& point, b2Transform& tf) const {
-    if(previousSegment== nullptr)
+    if (previousSegment == nullptr)
         return false;
     float distanceThis, distancePrev;
-    b2Vec2 normThis,normNext;
-    b2Transform transform=tf;
+    b2Vec2 normThis, normNext;
+    b2Transform transform = tf;
     ComputeDistance(transform, point, &distanceThis, &normThis, 0);
-    previousSegment->ComputeDistance(transform, point, &distancePrev, &normNext,0);
-    if (distancePrev==distanceThis){
+    previousSegment->ComputeDistance(transform, point, &distancePrev, &normNext, 0);
+    if (distancePrev == distanceThis) {
         return this > previousSegment;
     }
-    return distancePrev<distanceThis;
+    return distancePrev < distanceThis;
     //TODO
 }
 
 bool b2ArcShape::AddConnection(b2Shape& next) {
-    if (next.GetType()==b2Shape::Type::e_edge) {
-        b2EdgeShape edge = (b2EdgeShape &) next;
+    if (next.GetType() == b2Shape::Type::e_edge) {
+        b2EdgeShape edge = (b2EdgeShape&) next;
         if (edge.m_vertex1 == m_vertex1 || edge.m_vertex2 == m_vertex1) {
             previousSegment = &next;
             m_isLineSegment = true;
@@ -267,9 +235,8 @@ bool b2ArcShape::AddConnection(b2Shape& next) {
         } else {
             return false;
         }
-    }
-    else if(next.GetType()==b2Shape::Type::e_arc){
-            b2ArcShape arc =(b2ArcShape&) arc;
+    } else if (next.GetType() == b2Shape::Type::e_arc) {
+        b2ArcShape arc = (b2ArcShape&) arc;
         if (arc.m_vertex1 == m_vertex1 || arc.m_vertex2 == m_vertex1) {
             previousSegment = &next;
             m_isLineSegment = true;
@@ -281,10 +248,5 @@ bool b2ArcShape::AddConnection(b2Shape& next) {
         } else {
             return false;
         }
-    } else{ return false;}
-
-
-
-
-
+    } else { return false; }
 }
